1d3c655a305cf7029b1b37743b2267e1eb583bcf
soul <33748895+787449591@users.noreply.github.com>
2018-03-12 01:11:17 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+import numpy as np
+
+#xxx
+x_data = np.random.rand(100).astype(np.float32)
+y_data = x_data*0.2 + 0.5
+
+###  ###
+k = tf.Variable(tf.random_uniform([1],-1.0,1.0))
+b = tf.Variable(tf.zeros([1]))
+
+y = k*x_data +b
+
+loss = tf.reduce_mean(tf.square(y-y_data))
+optimizer = tf.train.GradientDescentOptimizer(0.5)
+train = optimizer.minimize(loss)
+
+init = tf.initialize_all_variables()
+
+###  ###
+
+sess = tf.Session()
+sess.run(init)
+
+for step in range(201):
+	sess.run(train)
+	if step % 20 == 0 :
+		print(step,sess.run(k),sess.run(b))
